bb5b048db9d7eac80691fb1101d25f5adcfdeaf8
hlmg <devhlmg@gmail.com>
2024-01-31 18:14:36 +0900

    Initial commit

---
+++ src/CartApp.java
@@ -0,0 +1,22 @@
+package src;
+
+public class CartApp {
+	public static void main(String[] args) {
+		// 상품 목록 생성
+		// Set<Product> productSet = new HashSet<>();
+		// TODO: 상품 생성, 상품 목록에 상품 추가
+
+		// 상품 목록 확인
+		System.out.println("고유한 상품 목록:");
+		// for (Product product : productSet) {
+		// 	System.out.println(product.getName() + " : " + product.getPrice());
+		// }
+
+		// 장바구니 생성
+		// Cart myCart = new Cart();
+
+		// TODO: 상품을 장바구니에 추가
+		// TODO: 상품을 장바구니에서 제거
+		// TODO: 장바구니에 현재 담긴 상품 출력 (상품 이름, 각 상품의 개수)
+	}
+}
